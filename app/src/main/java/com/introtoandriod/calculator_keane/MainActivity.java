358a0835e6520362440d44836cf20b5f5530b899
keanejc <keanejc@g.cofc.edu>
2017-02-23 20:53:52 -0500

    set up on save and on restore

--- app/src/main/java/com/introtoandriod/calculator_keane/MainActivity.java
+++ app/src/main/java/com/introtoandriod/calculator_keane/MainActivity.java
@@ -2,8 +2,10 @@ package com.introtoandriod.calculator_keane;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView view = (TextView) findViewById(R.id.textView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +14,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState( Bundle savedInstanceStare){
+    protected void onSaveInstanceState( Bundle savedInstanceState){
+        savedInstanceState.putCharSequence("view", view.getText());
 
     }
 
     @Override
     protected void onRestoreInstanceState( Bundle savedInstanceState){
-
+        view.setText(savedInstanceState.getCharSequence("view"));
     }
 }
