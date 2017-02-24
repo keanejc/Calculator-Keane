c164b51f3cd0542c43be1230b6a66f36b084363c
keanejc <keanejc@g.cofc.edu>
2017-02-23 20:45:12 -0500

    created alternative layouts TODO: button ids, onclick, everything else

--- app/src/main/java/com/introtoandriod/calculator_keane/MainActivity.java
+++ app/src/main/java/com/introtoandriod/calculator_keane/MainActivity.java
@@ -10,4 +10,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onSaveInstanceState( Bundle savedInstanceStare){
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState( Bundle savedInstanceState){
+
+    }
 }
